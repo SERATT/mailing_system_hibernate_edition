c2c92a494eddc049e0eb1580579abba2db34a347
Sayat <serat5915@gmail.com>
2023-06-26 16:49:39 +0600

    delete function changes

--- src/main/java/dev/seratt/mailing_system_hibernate_edition/dao/GroupDaoImpl.java
+++ src/main/java/dev/seratt/mailing_system_hibernate_edition/dao/GroupDaoImpl.java
@@ -1,2 +1,45 @@
-package dev.seratt.mailing_system_hibernate_edition.dao;public class GroupDaoImpl {
+package dev.seratt.mailing_system_hibernate_edition.dao;
+
+import dev.seratt.mailing_system_hibernate_edition.entity.GroupEntity;
+import dev.seratt.mailing_system_hibernate_edition.entity.UserEntity;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class GroupDaoImpl implements GroupDao {
+    @Autowired
+    EntityManager entityManager;
+    public List<GroupEntity> search(String searchText) {
+        return entityManager.createQuery("from GroupEntity where title like :searchText or description like :searchText ", GroupEntity.class)
+                .setParameter("searchText", "%"+searchText+"%")
+                .getResultList();
+    }
+
+    public GroupEntity findById(int id) {
+        return entityManager.find(GroupEntity.class, id);
+    }
+
+    public List<GroupEntity> findGroupsByUsersContaining(UserEntity userEntity) {
+        return entityManager.createQuery("SELECT g from GroupEntity g JOIN g.users u WHERE u.id = :userId", GroupEntity.class)
+                .setParameter("userId", userEntity.getId())
+                .getResultList();
+    }
+
+    public List<GroupEntity> findAll() {
+        return entityManager.createQuery("from GroupEntity ", GroupEntity.class)
+                .getResultList();
+    }
+
+    public void save(GroupEntity groupEntity) {
+        entityManager.merge(groupEntity);
+    }
+
+    public void deleteById(int id) {
+        entityManager.createQuery("UPDATE GroupEntity set isDeleted = true where id =:id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
 }
--- src/main/java/dev/seratt/mailing_system_hibernate_edition/dao/UserDaoImpl.java
+++ src/main/java/dev/seratt/mailing_system_hibernate_edition/dao/UserDaoImpl.java
@@ -1,2 +1,60 @@
-package dev.seratt.mailing_system_hibernate_edition.dao;public class UserDaoImpl {
+package dev.seratt.mailing_system_hibernate_edition.dao;
+
+import dev.seratt.mailing_system_hibernate_edition.entity.UserEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao{
+    @Autowired
+    EntityManager entityManager;
+
+    public List<UserEntity> search(String searchText) {
+        return entityManager.createQuery("from UserEntity where " +
+                "name like :searchText or " +
+                "surname like :searchText or " +
+                "otchestvo like :searchText or " +
+                "email like :searchText", UserEntity.class)
+                .setParameter("searchText", "%"+searchText+"%")
+                .getResultList();
+    }
+
+    @Override
+    public List<UserEntity> findAll() {
+        return entityManager.createQuery("from UserEntity where isDeleted = false order by id asc ", UserEntity.class).getResultList();
+    }
+
+    @Override
+    public void save(UserEntity userEntity) {
+        entityManager.merge(userEntity);
+    }
+
+    @Override
+    public UserEntity findById(int id) {
+        return entityManager.find(UserEntity.class, id);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        entityManager.createQuery("update UserEntity set isDeleted = true where id = :id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
+    @Override
+    public UserEntity findUserByEmail(String email) {
+        UserEntity userEntity;
+        try {
+            Query query = entityManager.createQuery("from UserEntity where email = :email", UserEntity.class)
+                    .setParameter("email", email);
+            userEntity = (UserEntity) query.getSingleResult();
+        } catch (Exception ex){
+            return null;
+        }
+        return userEntity;
+    }
 }
